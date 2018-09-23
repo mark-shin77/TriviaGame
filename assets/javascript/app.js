c99d7d44c70008753f01f58505d8abb5a543de8d
Mark <contact.mshin@gmail.com>
2018-09-23 17:47:24 -0600

    done with questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -14,73 +14,77 @@ $(document).ready(function() {
             question:"What is the name of Jon's direwolf?",
             choices: ["Grey Wind", "Lady", "Ghost", "Nymeria", "Summer", "Shaggydog"],
             correctAnswer: "Ghost",
-            image: "<img src='assets/images/ghost.jpg' class='jonWolf'>",
+            image: "<img src='../images/ghost.jpg' class='jonWolf'>",
         },
         {
             // Question 2
             question: "Which is not a Game of Thrones royal family name?",
             choices: ["Stark", "Tyrell", "Targaryen", "Baelish"],
             correctAnswer: "Baelish",
-            image: "<img src='assets/images/baelish.jpeg' class='baelish'>",
+            image: "<img src='../images/baelish.jpeg' class='baelish'>",
         },
         {
             // Question 3
             question: "Ayra's fighting style is called?",
             choices: ["Wolf Wield", "Water Dancing", "Stinger", "Slashing Steel"],
             correctAnswer:"Water Dancing",
-            image:"<img src='assets/images/arya.jpg' class='arya'>",
+            image:"<img src='../images/arya.jpg' class='arya'>",
         },
         {
             // Question 4
-            question: "",
-            choices: [],
-            correctAnswer: "",
-            image: "",
+            question: "Sandor Clegane is known as ...",
+            choices: ["The Hound", "The Kingslayer", "The Beast", "The Mountain"],
+            correctAnswer: "The Hound",
+            image: "<img src='../images/hound.jpg' class='hound'>",
         },
-        // {
-        //     // Question 5
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
-        // {
-        //     // Question 6
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
-        // {
-        //     // Question 7
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
-        // {
-        //     // Question 8
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
-        // {
-        //     // Question 9
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
-        // {
-        //     // Question 10
-        //     question:
-        //     choices:
-        //     correctAnswer:
-        //     image:
-        // },
+        {
+            // Question 5
+            question: "Bran Stark is paralyzed following a fall, who pushed him?",
+            choices: ["Tryon Lannister", "Jaime Lannister", "Cersei Lannister", "Joffrey Baratheon"],
+            correctAnswer: "Jamie Lannister",
+            image: "<img scr='../images/jaime.jpg' class='jaime'>",
+        },
+        {
+            // Question 6
+            question: "According to Bran, chaos is ...",
+            choices: ["a staircase", "a ladder", "inevitable", "a way of life"],
+            correctAnswer: "a ladder",
+            image: "<img scr='../images/bran.jpg' class='bran'>",
+        },
+        {
+            // Question 7
+            question: "What's the name of Arya Stark's sword?",
+            choices: ["Oathkeeper", "Ice", "Needle", "Death"],
+            correctAnswer: "Needle",
+            image: "<img scr='../images/needle.jpg' class='needle'>",
+        },
+        {
+            // Question 8
+            question: "What's House Lannister's motto?",
+            choices: ["Growing Strong", "Fire and Blood", "Hear Me Roar", "Winter is Coming"],
+            correctAnswer: "Hear Me Roar",
+            image: "<img scr='../images/lannister.jpg' class='lannister'>",
+        },
+        {
+            // Question 9
+            question: "Bran Stark has transformed into a seer called ...",
+            choices: ["The All Seeing", "The Three-Eyed Raven", "The Younger", "Hodor"],
+            correctAnswer: "The Three-Eyed Raven",
+            image: "<img scr='../images/raven.jpg' class='raven'>",
+        },
+        {
+            // Question 10
+            question: "What disease was Jorah Mormont stricken with?",
+            choices: ["Measles", "Greyscale", "Stone Skin", "Dragon Stone"],
+            correctAnswer: "Greyscale",
+            image: "<img scr='../images/greyscale.jpg' class='greyscale'>",
+        },
     ]
-    console.log(questions[2]);
+
+    // Test to see if array of questions was inputted correctly
+    for (var x = 0; x<questions.length; x++){
+        console.log(questions[x]);
+    }
 
 
 
